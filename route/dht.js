7e3031e0e4641e27460a646a7324ca0d7e6698ed
asmirnov <asmirnov@solit-clouds.ru>
2017-10-09 08:53:03 +0300

    add output

--- route/dht.js
+++ route/dht.js
@@ -1,23 +1,12 @@
 
 module.exports = {
-    options: function () {
-        return function(request, response) {
-            response.setHeader("Access-Control-Allow-Origin", "*");
-            response.setHeader("Access-Control-Allow-Headers", "*");
-            response.setHeader("Access-Control-Allow-Methods", "POST");
-            response.setHeader("Access-Control-Allow-Headers", "Content-Type");
-            response.sendStatus(200);
-        }
-    },
+   
     post: function(repository) {
         return function(request, response) {
 
+            console.log('body is:' + request.body)
             repository.save(request.body);
 
-            response.header("Access-Control-Allow-Origin", "*");
-            response.header("Access-Control-Allow-Headers", "*");
-            response.header("Access-Control-Allow-Methods", "POST");
-            response.header("Content-Type", "application/json;charset=UTF-8");
             response.sendStatus(200);
         }
     }
