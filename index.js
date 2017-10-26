f8d6203db115f5c7a7f2c562fd8ff4a285e14a91
asmirnov <asmirnov@solit-clouds.ru>
2017-10-26 20:58:27 +0300

    add get route

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ app.use(bodyParser.json());
 app.use(morgan('short'));
 
 //setup port
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 9999));
 
 //init routes
 const dhtRoute = require('./route/dht');
 app.post('/dht', dhtRoute.post(repository));
+app.get('/dht', (req, res) => res.send("hello world!!!"));
 
 //start server
 app.listen(app.get('port'), function () {
