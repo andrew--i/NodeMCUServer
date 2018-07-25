65355f92dd753cb1da7139320bc1ba1122fc7150
asmirnov <asmirnov@solit-clouds.ru>
2018-07-25 13:00:42 +0300

    change validation

--- bot/bot.js
+++ bot/bot.js
@@ -9,10 +9,12 @@ const usernames = process.env.TELEGRAM_USERNAMES ? process.env.TELEGRAM_USERNAME
 console.log('usernames: ' + usernames);
 
 function isValidMessage(message) {
-    if(!usernames || usernames.length === 0)
-        return true;
-    console.log('check user: ' + message.from.username);
-    return _.find(usernames, message.from.username);
+  if (!usernames || usernames.length === 0)
+    return true;
+  const username = message.from.username;
+  const isValid = usernames.indexOf(username) > -1;
+  console.log('check result for user: ' + username + '. isValid: ' + isValid);
+  return isValid;
 }
 
 
@@ -23,104 +25,104 @@ let period3 = {id: 6, name: "Прогноз"};
 let periods = [period2, period1, period3];
 
 function sendInitMessage(chatId) {
-    bot.sendMessage(chatId, 'Можно узнать температуру', {
-        "reply_markup": {
-            "keyboard": [periods.map(i => i.name)]
-        }
-    });
+  bot.sendMessage(chatId, 'Можно узнать температуру', {
+    "reply_markup": {
+      "keyboard": [periods.map(i => i.name)]
+    }
+  });
 }
 
 function normalizeNum(num) {
-    if (num < 10)
-        return '0' + num;
-    return num + '';
+  if (num < 10)
+    return '0' + num;
+  return num + '';
 }
 
 function formatDate(str) {
-    let date = new Date(str);
-    return [date.getDate(), date.getMonth() + 1, date.getFullYear()].map(i => normalizeNum(i)).join('-') + ' ' +
-        [date.getHours(), date.getMinutes(), date.getSeconds()].map(i => normalizeNum(i)).join(':')
+  let date = new Date(str);
+  return [date.getDate(), date.getMonth() + 1, date.getFullYear()].map(i => normalizeNum(i)).join('-') + ' ' +
+    [date.getHours(), date.getMinutes(), date.getSeconds()].map(i => normalizeNum(i)).join(':')
 }
 
 function isNow(period) {
-    return period.id === period1.id;
+  return period.id === period1.id;
 }
 
 function isYesterday(period) {
-    return period.id === period2.id;
+  return period.id === period2.id;
 }
 
 function isForecast(period) {
-    return period.id === period3.id;
+  return period.id === period3.id;
 }
 
 function sendNowPictures(repository, chatId) {
-    let currentDHT = repository.getDHT();
-    currentDHT.then(r => {
-        let time = formatDate(r.timestamp);
-        let chartsData = dht.getBarChartData(r.data);
+  let currentDHT = repository.getDHT();
+  currentDHT.then(r => {
+    let time = formatDate(r.timestamp);
+    let chartsData = dht.getBarChartData(r.data);
 
-        _.map(chartsData, c => {
-            chart.buffer(c).then(i => {
-                bot.sendPhoto(chatId, i, {caption: 'График на момент ' + time})
-            })
-        });
+    _.map(chartsData, c => {
+      chart.buffer(c).then(i => {
+        bot.sendPhoto(chatId, i, {caption: 'График на момент ' + time})
+      })
     });
+  });
 }
 
 function sendYesterdayPictures(repository, chatId) {
-    let yesterdayDHT = repository.getDHTForDay();
-    let yesterday = new Date(repository.getCurrentDate().getTime() - 24 * 60 * 60 * 1000);
-    let title = _.map([yesterday.getDate(), yesterday.getMonth(), yesterday.getFullYear()], normalizeNum).join('-');
-    yesterdayDHT.then(r => {
-        let charts = dht.getLineChartData(r, title);
-        _.map(charts, c => {
-            chart.buffer(c).then(i => {
-                bot.sendPhoto(chatId, i, {caption: 'График за вчера ( ' + title + ' )'});
-            })
-        })
-
+  let yesterdayDHT = repository.getDHTForDay();
+  let yesterday = new Date(repository.getCurrentDate().getTime() - 24 * 60 * 60 * 1000);
+  let title = _.map([yesterday.getDate(), yesterday.getMonth(), yesterday.getFullYear()], normalizeNum).join('-');
+  yesterdayDHT.then(r => {
+    let charts = dht.getLineChartData(r, title);
+    _.map(charts, c => {
+      chart.buffer(c).then(i => {
+        bot.sendPhoto(chatId, i, {caption: 'График за вчера ( ' + title + ' )'});
+      })
     })
+
+  })
 
 }
 
 function sendForecastMessage(weatherService, chatId) {
-    weatherService.getForecast()
-        .then(forecast => {
-            const head = 'Прогноз погоды на 10 дней от [Яндекса](https://yandex.ru/pogoda)';
-            const body = _.map(forecast, item => {
-                return '*' + item.date + '(' + item.dayOfWeek + ')*\t\t\t`' + item.day + '/' + item.night + '\t\t' + item.desc + '`'
-            });
+  weatherService.getForecast()
+    .then(forecast => {
+      const head = 'Прогноз погоды на 10 дней от [Яндекса](https://yandex.ru/pogoda)';
+      const body = _.map(forecast, item => {
+        return '*' + item.date + '(' + item.dayOfWeek + ')*\t\t\t`' + item.day + '/' + item.night + '\t\t' + item.desc + '`'
+      });
 
 
-            bot.sendMessage(chatId, [head, body.join('\n\n')].join('\n\n'), {
-                parse_mode: "markdown"
-            });
-        });
+      bot.sendMessage(chatId, [head, body.join('\n\n')].join('\n\n'), {
+        parse_mode: "markdown"
+      });
+    });
 
 }
 
 module.exports = function (repository, weatherService) {
-    bot.on('message', (msg) => {
-        const chatId = msg.chat.id;
-        if (isValidMessage(msg)) {
+  bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    if (isValidMessage(msg)) {
 
-            let period = _.find(periods, p => p.name === msg.text);
-            if (period) {
-                if (isNow(period)) {
-                    sendNowPictures(repository, chatId);
-                } else if (isYesterday(period)) {
-                    sendYesterdayPictures(repository, chatId);
-                } else if (isForecast(period)) {
-                    sendForecastMessage(weatherService, chatId)
-                } else {
-                    bot.sendMessage(chatId, "Еще в разработки, ожидайте позже");
-                    sendInitMessage(chatId);
-                }
-            } else {
-                sendInitMessage(chatId);
-            }
-
+      let period = _.find(periods, p => p.name === msg.text);
+      if (period) {
+        if (isNow(period)) {
+          sendNowPictures(repository, chatId);
+        } else if (isYesterday(period)) {
+          sendYesterdayPictures(repository, chatId);
+        } else if (isForecast(period)) {
+          sendForecastMessage(weatherService, chatId)
+        } else {
+          bot.sendMessage(chatId, "Еще в разработки, ожидайте позже");
+          sendInitMessage(chatId);
         }
-    });
+      } else {
+        sendInitMessage(chatId);
+      }
+
+    }
+  });
 };
